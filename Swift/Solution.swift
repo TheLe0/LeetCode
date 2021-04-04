a49fb3436c32e39bda9fa9087298d56bea519a13
Leonardo B. Tosin <40045069+TheLe0@users.noreply.github.com>
2021-04-04 20:18:41 -0300

    Update Solution.swift

--- Swift/Solution.swift
+++ Swift/Solution.swift
@@ -614,5 +614,23 @@ class Solution {
         let upper = chars.filter({ $0 <= zVal }).count
         return (upper==0) || (upper==chars.count) || ((upper==1) && (chars[0] <= zVal))
     }
-    
+	
+    /**
+    *  242 - Valid Anagram
+    *  https://leetcode.com/problems/valid-anagram/
+    */
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        
+        guard s.count == t.count else { return false }
+        
+        var map = [Character:Int]()
+        s.forEach({ map[$0, default: 0] += 1 })
+        t.forEach({ map[$0, default: 0] -= 1 })
+        
+        for (_,v) in map {
+            if v != 0 { return false }
+        }
+
+        return true
+    }
 }
